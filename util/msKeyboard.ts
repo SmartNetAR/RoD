3ac932dfdf84bf2abca3851acbb10e2d51f97e67
Leonardo Casales <leonardo@smartnet.com.ar>
2023-06-04 19:28:00 -0300

    adding keyboard input control

--- util/msKeyboard.ts
+++ util/msKeyboard.ts
@@ -1,53 +1,95 @@
+const keyMap = {
+  down: false,
+  up: false,
+  right: false,
+  left: false,
+}
+
+type MapKey = keyof typeof keyMap;
 
 class MsKeyboard {
-
-  static keyMap = {
-    down: false,
-    up: false,
-    right: false,
-    left: false,
-  }
 
   static init() {
     MsKeyboard.addEventListeners();
   }
 
+  static isKeyPressed(key: MapKey): Boolean {
+    return keyMap[key];
+  };
   // static onkeypress(k: any) { };
   // static onkeyrelease(k: any) { };
   // static isKeyPressed(k: any) { };
   // static isExitKeyPressed() { };
 
   private static addEventListeners() {
+    // buttons
     const controlButtons = document.querySelectorAll('.controls button');
 
     for (const ctrlBtn of controlButtons) {
-      ctrlBtn.addEventListener('mousedown', (ev) => this.btnHandler(ev));
-      ctrlBtn.addEventListener('mouseup', (ev) => this.btnHandler(ev));
-      ctrlBtn.addEventListener('touchstart', (ev) => this.btnHandler(ev));
-      ctrlBtn.addEventListener('touchend', (ev) => this.btnHandler(ev));
+      ctrlBtn.addEventListener('mousedown', this.buttonPressEventHandler.bind(this));
+      ctrlBtn.addEventListener('mouseup', this.buttonReleaseEventHandler.bind(this));
+      ctrlBtn.addEventListener('mouseleave', this.buttonReleaseEventHandler.bind(this));
+      ctrlBtn.addEventListener('touchstart', this.buttonPressEventHandler.bind(this));
+      ctrlBtn.addEventListener('touchend', this.buttonReleaseEventHandler.bind(this));
     }
+
+    // keyboard
+    const body = document.body;
+
+    body.addEventListener('keydown', this.keyPressEventHandler.bind(this));
+    body.addEventListener('keyup', this.keyReleaseEventHandler.bind(this));
+
   }
 
-  private static btnHandler(ev: Event) {
-    const btnName = (ev.target as HTMLButtonElement).name;
+  private static mapKeyboardKeyToArrow(ev: KeyboardEvent): MapKey {
+    const keyboardKeys = {
+      'ArrowDown': 'down',
+      'ArrowUp': 'up',
+      'ArrowRight': 'right',
+      'ArrowLeft': 'left',
+      's': 'down',
+      'w': 'up',
+      'd': 'right',
+      'a': 'left',
+    }
+    return keyboardKeys[ev.key];
+  }
 
-    const arrow = btnName;
-    const type = ev.type;
-
-    if (this.keyMap[arrow] === undefined) {
+  private static pressKey(arrow: string) {
+    if (keyMap[arrow] === undefined) {
       return;
     }
-
-    if (type === 'mousedown' || type === 'touchstart') {
-      this.keyMap[arrow] = true;
-    } else if (type === 'mouseup' || type === 'touchend') {
-      this.keyMap[arrow] = false;
-    }
+    keyMap[arrow] = true;
   }
 
-  static isKeyPressed(key: keyof typeof MsKeyboard.keyMap): Boolean {
-    return this.keyMap[key];
-  };
+  private static releaseKey(arrow: string) {
+    if (keyMap[arrow] === undefined) {
+      return;
+    }
+    keyMap[arrow] = false;
+  }
+
+  private static keyPressEventHandler(ev: KeyboardEvent) {
+    ev.preventDefault();
+    const arrow = this.mapKeyboardKeyToArrow(ev)
+    this.pressKey(arrow);
+  }
+
+  private static keyReleaseEventHandler(ev: KeyboardEvent) {
+    ev.preventDefault();
+    const arrow = this.mapKeyboardKeyToArrow(ev)
+    this.releaseKey(arrow);
+  }
+
+  private static buttonPressEventHandler(ev: Event) {
+    const arrow = (ev.target as HTMLButtonElement).name;
+    this.pressKey(arrow);
+  }
+
+  private static buttonReleaseEventHandler(ev: Event) {
+    const arrow = (ev.target as HTMLButtonElement).name;
+    this.releaseKey(arrow);
+  }
 }
 
 export default MsKeyboard;
